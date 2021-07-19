9e2e4ea9035ba6d56ad2418fb3dc169e67196432
EvgenyBelichkov <70999622+EvgenyBelichkov@users.noreply.github.com>
2021-07-19 08:31:07 +0300

    Added 1 more test with empty dictionary

--- tests/test_menu_printer_class.py
+++ tests/test_menu_printer_class.py
@@ -40,3 +40,14 @@ def test_class_MenuPrinter(capsys, dictionary, expected_result):
     out, err = capsys.readouterr()
     assert out == '/welcome\n/dishes/main/fish\n/dishes/main/meat\n/dishes/salads\n'
     assert err == ''
+
+
+def test_class_MenuPrinter_with_empty_dict(capsys):
+    class Menu:
+        def getItems(self):
+            return {}
+    menu = Menu()
+    menu_printer = MenuPrinter(menu)
+    menu_printer.print_menu()
+    out, err = capsys.readouterr()
+    assert out == ''
