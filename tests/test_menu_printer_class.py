d74cfbbef9631d9dc078818e33de03e937af40a0
EvgenyBelichkov <70999622+EvgenyBelichkov@users.noreply.github.com>
2021-07-19 08:34:37 +0300

    Tests updated

--- tests/test_menu_printer_class.py
+++ tests/test_menu_printer_class.py
@@ -27,8 +27,15 @@ example_1 = {
             # Here comes more and more
         }
 
+example_2 = {
+            'welcome': {
+                'caption': 'Welcome',
+            },
+        }
 
-@pytest.mark.parametrize("dictionary, expected_result", [(example_1, '/welcome\n/dishes/main/fish\n/dishes/main/meat\n/dishes/salads\n')])
+
+@pytest.mark.parametrize("dictionary, expected_result", [(example_1, '/welcome\n/dishes/main/fish\n/dishes/main/meat\n/dishes/salads\n'),
+                                                         (example_2, '/welcome\n')])
 def test_class_MenuPrinter(capsys, dictionary, expected_result):
     class Menu:
         def getItems(self):
@@ -38,7 +45,7 @@ def test_class_MenuPrinter(capsys, dictionary, expected_result):
     menu_printer = MenuPrinter(menu)
     menu_printer.print_menu()
     out, err = capsys.readouterr()
-    assert out == '/welcome\n/dishes/main/fish\n/dishes/main/meat\n/dishes/salads\n'
+    assert out == expected_result
     assert err == ''
 
 
